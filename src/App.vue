3cd9cdbee3eb1794ad133ad353a803968be9799e
KamilGitHubowiec <hantsky123@gmail.com>
2021-05-29 12:07:47 +0200

    Add Meetings functionality

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
   <div>
     <h1>Witaj w systemie do zapisów na zajęcia</h1>
     <login-form v-if="isLoggedIn === false" @login="logUserIn($event)"></login-form>
-    <logged-in-view v-if="isLoggedIn === true" @logout="logUserOut($event)" :user-email="email"></logged-in-view>
+    <div v-if="isLoggedIn === true">
+      <logged-in-view @logout="logUserOut($event)" :user-email="email"></logged-in-view>
+      <meeting-page></meeting-page>
+    </div>
   </div>
 </template>
 
@@ -10,11 +13,13 @@
 import "milligram";
 import LoginForm from "./components/LoginForm";
 import LoggedInView from './components/LoggedInView.vue';
+import MeetingPage from './components/MeetingPage.vue';
 
 export default {
   components: {
     LoginForm,
-    LoggedInView
+    LoggedInView,
+    MeetingPage
   },
 
   data() {
